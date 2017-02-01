073fb0dee1175aa7a7bda80bb3feb3210e95d092
jholm117 <jholm117@gmail.com>
2017-02-01 13:59:37 -0600

    foundation

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+    public float health = 1f;       //enemy starting health
+    
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(health <= 0f)
+        {
+            Destroy(gameObject);
+        }
+	}
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if(coll.gameObject.tag == "Projectile")
+        {
+            health -= 1f;
+        }
+    }
+}
