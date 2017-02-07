0233d91e71b179fc8464513b9f93dbf7e7669549
tpierce14 <tpierce14@optonline.net>
2017-02-07 17:29:17 -0600

    fixed the walls and projectiles not interacting

---
+++ GameStudioGame/Assets/Scripts/Ground.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ground : MonoBehaviour
+{
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        return;
+    }
+}
