e0308206c45f7f59ea4ea15244682780ae4b2ee6
jvaldillez <jayvaldillez2018@u.northwestern.edu>
2017-02-27 15:23:30 -0600

    stealth-abilities

---
+++ GameStudioGame/assets/Scripts/Spells/ChokeHold.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChokeHold : Ability
+{
+
+    //private float timer;            // counts lifetime
+    private PlayerController player;
+    private Enemy victim;             //Enemy we hit
+
+    private Animator animator;
+
+    void Awake()
+    {
+
+        offset = 0.5f;
+        //Invoke("Destruct", lifeTime);
+    }
+
+
+    void Update()
+    {
+
+        Destruct();
+
+
+    }
+
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        if (coll.tag == "Enemy")
+        {
+            victim = coll.GetComponent<Enemy>();
+
+            //immobilize enemy
+            immobilize(victim);
+
+            //put animation here? or in enemy?
+            //animator.SetTrigger("enemyDead");
+
+            //lifetime
+            //Invoke("Destruct", 1f);
+        }
+    }
+
+    public override void Destruct()
+    {
+        FindObjectOfType<PlayerController>().Mobile = true;
+        base.Destruct();
+    }
+
+    public override void Init(CharacterTemplate chr)
+    {
+        //var player = FindObjectOfType<PlayerController>().transform;
+        //transform.parent = player;
+        player = chr.GetComponent<PlayerController>();
+        //immobilize(chr);
+        base.Init(chr);
+    }
+    
+
+
+}
---
+++ GameStudioGame/assets/Scripts/Spells/Silence.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Silence : Ability
+{
+
+    private float timer;            // counts lifetime
+    private PlayerController player;
+
+
+    void Start()
+    {
+        player = FindObjectOfType<PlayerController>();
+        timer = 0f;
+    }
+    void Update()
+    {
+
+
+
+        // destroy after lifetime seconds and revert radius to normal
+        timer += Time.deltaTime;
+        if (timer >= lifeTime)
+        {
+
+
+            foreach (Enemy enemy in FindObjectsOfType<Enemy>())
+            {
+                enemy.distanceThreshold = 5f;
+            }
+
+            Destruct();
+
+        }
+
+        else
+        {
+            foreach (Enemy enemy in FindObjectsOfType<Enemy>())
+            {
+                enemy.distanceThreshold = 0f;
+            }
+        }
+
+
+    }
+
+
+
+
+    //want to destruct after time out 
+    public override void Destruct()
+    {
+        //FindObjectOfType<PlayerController>().Mobile = true;
+        base.Destruct();
+    }
+
+    public override void Init(CharacterTemplate chr)
+    {
+        //var player = FindObjectOfType<PlayerController>().transform;
+        //transform.parent = player;
+        player = chr.GetComponent<PlayerController>();
+        base.Init(chr);
+    }
+
+}
