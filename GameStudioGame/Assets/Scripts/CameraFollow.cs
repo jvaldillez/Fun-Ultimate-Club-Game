aeec460460bb97f52575ad84bd374bf30de66c22
tpierce14 <tpierce14@optonline.net>
2017-02-28 19:18:29 -0600

    smooted camera

--- GameStudioGame/Assets/Scripts/CameraFollow.cs
+++ GameStudioGame/Assets/Scripts/CameraFollow.cs
@@ -6,8 +6,8 @@ public class CameraFollow : MonoBehaviour
 {
     public float xMargin = 1f;		// Distance in the x axis the player can move before the camera follows.
     public float yMargin = 1f;		// Distance in the y axis the player can move before the camera follows.
-    public float xSmooth = 20f;		// How smoothly the camera catches up with it's target movement in the x axis.
-    public float ySmooth = 20f;		// How smoothly the camera catches up with it's target movement in the y axis.
+    public float xSmooth = 200f;		// How smoothly the camera catches up with it's target movement in the x axis.
+    public float ySmooth = 200f;		// How smoothly the camera catches up with it's target movement in the y axis.
     //public Vector2 maxXAndY;		// The maximum x and y coordinates the camera can have.
     //public Vector2 minXAndY;		// The minimum x and y coordinates the camera can have.
 
