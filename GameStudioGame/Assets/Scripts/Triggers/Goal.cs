d16510dc56cdf108b92eb9b6280fb617effe0122
tpierce14 <tpierce14@optonline.net>
2017-03-06 11:53:29 -0600

    added level linking and prefabs

--- GameStudioGame/Assets/Scripts/Triggers/Goal.cs
+++ GameStudioGame/Assets/Scripts/Triggers/Goal.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Goal : MonoBehaviour
 {
     public Text gameOverText;
+    public string newScene;
 
     void OnTriggerEnter2D(Collider2D coll)
     {
@@ -16,6 +18,8 @@ public class Goal : MonoBehaviour
             var playerRb = player.GetComponent<Rigidbody2D>();
             playerRb.velocity = new Vector3(0, 0, 0);      
             gameOverText.text = "Level Complete";
+            //Application.LoadLevel(newScene);
+            SceneManager.LoadScene(newScene);
         }
     }
 }
